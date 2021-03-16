b189e2ba17b6b8e353872edf198acce2e9ad65e0
Krzysztof Sosnowski <kjksosnowski@gmail.com>
2021-03-16 19:28:39 +0100

    test

--- main.cpp
+++ main.cpp
@@ -1,15 +1,45 @@
 #include <iostream>
+#include <stdio.h>
 #include "functions.h"
 
 //int sumc(int a, int b);
 //string CustomText(string input, colors Fcolor, colors Bcolor,  string options);
 using namespace std;
 
+char gfx[80*25];
 
+void put_char(int x, int y, char c) {
+    gfx[x+y*80] = c;
+}
 
+void clear_buffer() {
+    for(int i = 0; i < 80*25; i += 1)
+        gfx[i] = ' ';
+}
+
+void refresh_screen() {
+    printf("\033[2J\033[1;1H");
+    printf("%s", gfx);
+}
 
 int main() {
+    int x = 5, y = 5;
+
+    while(1) {
+        clear_buffer();
+        put_char(x, y, 219);
+        x += 1;
+        y += 1;
+        refresh_screen();
+        gets(gfx);
+    }
+
+    return 0;
+}
+
+
+/*int main() {
     cout << "Hello World!\n";
     cout << CustomText("red", red, black, "");
     return 0;
-}
+}*/
