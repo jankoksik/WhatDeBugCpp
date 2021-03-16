8127ce85a365994b78989daf187ebd75f282e4e1
Krzysztof Sosnowski <kjksosnowski@gmail.com>
2021-03-16 19:42:03 +0100

    ws

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 #include <stdio.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
 #include "functions.h"
 
 //int sumc(int a, int b);
 //string CustomText(string input, colors Fcolor, colors Bcolor,  string options);
 using namespace std;
+
+#define GFX_WIDTH
+
 
 char gfx[80*25];
 
@@ -25,14 +30,20 @@ void refresh_screen() {
 int main() {
     int x = 5, y = 5;
 
-    while(1) {
+    /*while(1) {
         clear_buffer();
         put_char(x, y, 219);
         x += 1;
         y += 1;
         refresh_screen();
         gets(gfx);
-    }
+    }*/
+
+    struct winsize w;
+    ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+
+    printf ("lines %d\n", w.ws_row);
+    printf ("columns %d\n", w.ws_col);
 
     return 0;
 }
